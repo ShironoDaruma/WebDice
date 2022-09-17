f274b96b7cc4483f2d380a7a976184bf330ca514
ShironoDaruma <shirono.daruma@gmail.com>
2022-09-17 14:39:56 +0900

    [add] connect database

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php
-  function NewPage($name) {
-    $file = "rooms/" . $name;
-    if(file_exists($file)) {
-      return false;
-    } else {
-      touch()
-    }
+  function console_log($data){
+    echo '<script>';
+    echo 'console.log('.json_encode($data).')';
+    echo '</script>';
   }
+
+  $dsn = 'mysql:host=mysql1.php.xdomain.ne.jp;dbname=shirodaruma_webdice';
+  $username = 'shirodaruma_dev1';
+  $password = 'dev1DEV1';
+
+  try {
+    $db = new PDO($dsn, $username, $password);
+    console_log("db接続成功");
+  } catch (PDOException $e) {
+    console_log("db接続失敗: ".$e->getMessage());
+    exit();
+  }
+
 ?>
 
 <!DOCTYPE html>
@@ -21,15 +31,17 @@
       <h2><a href="#"><span class="textRed">WebDice</span></a></h1>
     </div>
     <div class="title">
-      <h1><span class="textRed">WebDice</span></h1>
-      
-      <p><span class="textWhite">簡単な操作でダイスをロールできる会員登録不要のオンラインダイスツールです。</span></p>
-      <p><span class="textWhite">ルームURLを自由に決めて、ダイス結果をリアルタイムで皆に共有できます。</span></p><br>
+      <form action="" method="POST">
+        <h1><span class="textRed">WebDice</span></h1>
 
-      <p><span class="textWhite">https://shirodaruma.php.xdomain.jp/WebDice/rooms/</span><input type="url" id="url" name="url" placeholder="ルームURLを入力"></p>
-      <p><span class="textWhite">パスワードを入力</span><input type="password" id="password" name="password"></p>
-      <input type="submit" id="submit" name="submit" value="新規ルーム" onclick="<?php ?>">
-      <input type="submit" id="submit" name="submit" value="ルーム入室">
+        <p><span class="textWhite">簡単な操作でダイスをロールできる会員登録不要のオンラインダイスツールです。</span></p>
+        <p><span class="textWhite">ルームURLを自由に決めて、ダイス結果をリアルタイムで皆に共有できます。</span></p><br>
+
+        <p><span class="textWhite">https://shirodaruma.php.xdomain.jp/WebDice/rooms/</span><input type="url" id="url" name="url" placeholder="ルームURLを入力"></p>
+        <p><span class="textWhite">パスワードを入力</span><input type="password" id="password" name="password"></p>
+        <input type="submit" id="submit" name="newroom" value="新規ルーム">
+        <input type="submit" id="submit" name="roomIn" value="ルーム入室">
+      </form>
     </div>
   </body>
 </html>
